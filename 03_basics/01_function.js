7a8aa7e8e697ca04434aaa4e515b718d8518c0ec
Hardik Babla <devhardikbabla@gmail.com>
2026-08-03 16:20:58 +0530

    Function with Objects

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -52,4 +52,47 @@ function loginUser(userName = "Guest") {
 
 // console.log(loginUser("Hardik"));
 
-console.log(loginUser());
+// console.log(loginUser());
+
+// ################################################################
+
+// Rest Operator (...)
+
+function calculateTotalPrice(firstItem, secondItem, ...remainingItems) {
+  return remainingItems;
+}
+
+// console.log(calculateTotalPrice(500));
+// console.log(calculateTotalPrice(500, 1000, 1500, 2000, 2500));
+
+// Object as a Function Argument
+
+const student = {
+  name: "Hardik",
+  age: 20,
+};
+
+function printStudent(studentInfo) {
+  console.log(
+    `Name: ${studentInfo.name} | Age: ${studentInfo.age}`
+  );
+}
+
+// printStudent(student);
+
+printStudent({
+  name: "Rahul",
+  age: 21,
+});
+
+// Array as a Function Argument
+
+const marks = [85, 90, 95, 98];
+
+function getThirdValue(arr) {
+  return arr[2];
+}
+
+// console.log(getThirdValue(marks));
+
+console.log(getThirdValue([80, 85, 90, 95]));
