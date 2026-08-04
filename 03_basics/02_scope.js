8cf8a53f682bfc1573c86cedc7523bbd600dc10d
Hardik Babla <devhardikbabla@gmail.com>
2026-08-04 16:05:16 +0530

    Scope level and mini hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -21,3 +21,77 @@ console.log("Outside Block:", maxStudents);
 
 // console.log(totalCourses); // ❌ Not Accessible Outside the Block
 // console.log(totalUsers);   // Accessible because 'var' is function-scoped
+
+// ################################################################
+
+// Nested Functions
+
+/*
+A nested function can access variables
+from its parent function.
+
+However, the parent function cannot
+access variables declared inside the
+nested function.
+*/
+
+function displayUser() {
+  const userName = "Hardik";
+
+  function displayWebsite() {
+    const websiteName = "GitHub";
+
+    console.log(userName);
+  }
+
+  // console.log(websiteName); // ❌ Not Accessible Here
+
+  displayWebsite();
+}
+
+displayUser();
+
+// Block Scope
+
+if (true) {
+  const userName = "Hardik";
+
+  if (userName === "Hardik") {
+    const websiteName = "GitHub";
+
+    console.log(userName + " " + websiteName);
+  }
+
+  // console.log(websiteName); // ❌ Not Accessible Here
+}
+
+// console.log(userName); // ❌ Not Accessible Here
+
+
+// Function Hoisting
+
+/*
+Function declarations are hoisted,
+so they can be called before they
+are defined.
+
+Function expressions are NOT hoisted
+in the same way because the variable
+is initialized later.
+*/
+
+console.log(addOne(2));
+
+function addOne(number) {
+  return number + 1;
+}
+
+// console.log(addOne(2));
+
+console.log(addTwo(5));
+
+const addTwo = function (number) {
+  return number + 2;
+};
+
+// console.log(addTwo(5));
